1fe9577d056a89a101128c7e4d4243d34fc178a6
Maxime Cousinou <maxime.cousinou@zemax.net>
2014-01-17 15:46:29 +0100

    more Social modules

---
+++ MF/Social/Instagram.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maxime
+ * Date: 17/01/14
+ * Time: 15:23
+ */
+
+namespace MF\Social;
+
+
+class Instagram {
+	public function __construct($accessToken) {
+		$this->accessToken = $accessToken;
+	}
+
+	public static function getAccessTokenURL($clientId, $redirectURL) {
+		$url = 'https://instagram.com/oauth/authorize/'
+					.'?client_id='.$clientId
+					.'&redirect_uri='.urlencode($redirectURL)
+					.'&response_type=token';
+
+		return $url;
+	}
+
+	/****************************************************************************************************
+	 * ACTIONS
+	 ****************************************************************************************************/
+
+	public function searchByTag($tag, $max_id = '', $min_id = '') {
+		$url = 'https://api.instagram.com/v1/tags/'.$tag.'/media/recent'
+			.'?access_token='.$this->accessToken;
+
+		if (!empty($max_id)) {
+			$url .= '&max_id='.$max_id;
+		}
+
+		if (!empty($min_id)) {
+			$url .= '&min_id='.$min_id;
+		}
+
+		$ch = curl_init();
+		curl_setopt ($ch, CURLOPT_URL, $url);
+		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+		$response = curl_exec($ch);
+
+		$grams = json_decode($response);
+
+		return $grams;
+	}
+} 
--- MF/Social/Twitter.php
+++ MF/Social/Twitter.php
@@ -12,26 +12,40 @@ include_once 'twitteroauth\twitteroauth.php';
 
 class Twitter {
 	public function __construct($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret) {
-		$this->consumerKey 			= $consumerKey;
-		$this->consumerSecret 		= $consumerSecret;
-		$this->accessToken 			= $accessToken;
-		$this->accessTokenSecret 	= $accessTokenSecret;
-	}
+		$this->consumerKey = $consumerKey;
+		$this->consumerSecret = $consumerSecret;
+		$this->accessToken = $accessToken;
+		$this->accessTokenSecret = $accessTokenSecret;
 
-/****************************************************************************************************
- * ACTIONS
- ****************************************************************************************************/
-
-	public function getUserTweets($user, $count = 20, $include_rts = false, $ignore_replies = true) {
-		$connection = new \TwitterOAuth(
+		$this->connection = new \TwitterOAuth(
 			$this->consumerKey,
 			$this->consumerSecret,
 			$this->accessToken,
 			$this->accessTokenSecret
 		);
+	}
 
-		$tweets = $connection->get(
-			"https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" . $user . "&count=" . $count . "&include_rts=" . $include_rts . "&exclude_replies=" . $ignore_replies
+	/****************************************************************************************************
+	 * ACTIONS
+	 ****************************************************************************************************/
+
+	public function getUserTweets($user, $count = 20, $include_rts = false, $ignore_replies = true) {
+		$tweets = $this->connection->get(
+			'https://api.twitter.com/1.1/statuses/user_timeline.json'
+						.'?screen_name='.$user
+						.'&count='.$count
+						.'&include_rts='.$include_rts
+						.'&exclude_replies='.$ignore_replies
+		);
+
+		return $tweets;
+	}
+
+	public function searchTweets($q, $count = 20) {
+		$tweets = $this->connection->get(
+			'https://api.twitter.com/1.1/search/tweets.json'
+						.'?q='.urlencode($q)
+						.'&count='.$count
 		);
 
 		return $tweets;
