e8af9aabeaa15d7efbbc7d94cd7ed55d7539ed25
Maxime Cousinou <maxime.cousinou@zemax.net>
2014-01-27 14:46:31 +0100

    Add default Charset to iso-8859-15

--- MF/Email/Mailer.php
+++ MF/Email/Mailer.php
@@ -4,5 +4,7 @@ namespace MF\Email;
 require_once('phpmailer/class.phpmailer.php');
 
 class Mailer extends \PHPMailer {
-	
+	public function __construct() {
+		$this->CharSet = 'iso-8859-15';
+	}
 }
