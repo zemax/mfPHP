f33cc4e8eb03a9c81174dc8ff8274da3e000d5a1
Maxime Cousinou <maxime.cousinou@zemax.net>
2014-09-09 10:10:35 +0200

    Add GET array parameter in Redirect

--- MF/Response.php
+++ MF/Response.php
@@ -1,83 +1,90 @@
 <?php
 /**
-* Response
-* 
-* @version 0.1.2
-*/
+ * Response
+ *
+ * @version 0.1.2
+ */
 
 namespace MF;
 
 class Response {
-	public static $contentTypeHTML 			= 'text/html';
-	public static $contentTypeTEXT 			= 'text/plain';
-	public static $contentTypeJAVASCRIPT 	= 'text/javascript';
-	public static $contentTypeCSS 			= 'text/css';
-	public static $contentTypeCSV 			= 'text/csv';
-	public static $charsetUTF8 				= 'utf-8';
-	public static $charsetISO 				= 'iso-8859-1';
-	
+	public static $contentTypeHTML = 'text/html';
+	public static $contentTypeTEXT = 'text/plain';
+	public static $contentTypeJAVASCRIPT = 'text/javascript';
+	public static $contentTypeCSS = 'text/css';
+	public static $contentTypeCSV = 'text/csv';
+	public static $charsetUTF8 = 'utf-8';
+	public static $charsetISO = 'iso-8859-1';
+
 	/**
 	 * Send no cache header
 	 */
 	public static function setNoCache () {
-		header("Cache-Control: no-cache, must-revalidate");
-		header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+		header( "Cache-Control: no-cache, must-revalidate" );
+		header( "Expires: Mon, 26 Jul 1997 05:00:00 GMT" );
 	}
-	
+
 	/**
 	 * Send redirection header
-	 * 
+	 *
 	 * @param $url
 	 */
-	public static function setRedirect ($url) {
-		header('Location: '.$url);
+	public static function setRedirect ( $url, $get = array() ) {
+		if ( !empty( $get ) ) {
+			$url .= '?';
+		}
+		foreach ( $get as $key => $value ) {
+			$url .= $key . '=' . urlencode( $value ) . '&';
+		}
+
+		header( 'Location: ' . $url );
 		exit();
 	}
-	
+
 	/**
 	 * Send Content Type header
-	 * 
-	 * @param $type		String
-	 * @param $charset	String
+	 *
+	 * @param $type        String
+	 * @param $charset    String
 	 */
-	public static function setContentType ($type, $charset = '') {
-		if (!empty($charset)) {
-			header('Content-Type: '.$type.'; charset='.$charset);
+	public static function setContentType ( $type, $charset = '' ) {
+		if ( !empty( $charset ) ) {
+			header( 'Content-Type: ' . $type . '; charset=' . $charset );
 		}
 		else {
-			header('Content-Type: '.$type);
+			header( 'Content-Type: ' . $type );
 		}
 	}
 
 	/**
 	 * Send P3P Policy
 	 *
-	 * @param $p3p		String
+	 * @param $p3p        String
 	 */
-	public static function setP3P ($p3p = 'CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT"') {
-		header('P3P: '.$p3p);
+	public static function setP3P ( $p3p = 'CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT"' ) {
+		header( 'P3P: ' . $p3p );
 	}
 
 	/**
 	 * Send a String with gzip
-	 * 
-	 * @param $output	String
-	 * @param $gzip		Boolean
+	 *
+	 * @param $output    String
+	 * @param $gzip        Boolean
 	 */
-	public static function sendOutput ($output, $gzip = false) {
-		if ($gzip) {
-			ob_start("ob_gzhandler");
+	public static function sendOutput ( $output, $gzip = false ) {
+		if ( $gzip ) {
+			ob_start( "ob_gzhandler" );
 		}
-		
-		 print ($output);
+
+		print ( $output );
 	}
-	
-	public static function setHTTPStatus ($code = 200) {
-		switch ($code) {
+
+	public static function setHTTPStatus ( $code = 200 ) {
+		switch ( $code ) {
 			case 404:
-				header("HTTP/1.0 404 Not Found");
+				header( "HTTP/1.0 404 Not Found" );
 				break;
-				
+
 			default:
 		}
 	}
